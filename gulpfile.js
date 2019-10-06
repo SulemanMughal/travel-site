3c317c8f53822d6988fa89a040880ff185f9c464
SulemanMughal <sulemanmughal155@gmail.com>
2019-10-06 15:23:09 +0500

    Fifth

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
 var gulp = require('gulp');
-gulp.task('default', async function()
-{
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+
+gulp.task('default', async function () {
     console.log('Hurray!');
 });
+
+gulp.task('html', async function () {
+    console.log('HTML has been changes....');
+});
+
+gulp.task('styles', async function () {
+    // console.log('Style has been changes....');
+    return gulp.src('../app/assets/styles/style.css')
+        .pipe(postcss([cssvars, autoprefixer]))
+        .pipe(gulp.dest('../app/temp/styles'));
+});
+
+gulp.task('watch', function () {
+    watch('../app/index.html', gulp.series('html'));
+    watch('../app/assets/styles/**/*.css', gulp.series('styles'));
+});
+
+// exports.build = gulp.series(html, watch);
